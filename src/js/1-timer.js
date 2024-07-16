6a4a02b3fd99bdcded597d3db8713eee541f30c1
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-07-16 12:28:04 +0300

    update

---
+++ src/js/1-timer.js
@@ -0,0 +1,23 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  inputEl: document.querySelector('datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  clockface: document.querySelector('.js-clockface'),
+};
+
+flatpickr(inputEl, options);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
