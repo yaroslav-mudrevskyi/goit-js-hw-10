2f3c7692acd0069e0cbb85b3a09c1e74fdcf270d
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-07-17 18:26:07 +0300

    timer done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -10,51 +10,77 @@ const refs = {
   timerEl: document.querySelector('.timer'),
 };
 
+const elements = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.setAttribute('disabled', '');
+
+let userSelectedDate;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      refs.startBtn.setAttribute('disabled', '');
+      iziToast.show({
+        title: 'Oops',
+        titleColor: 'white',
+        message: 'Please choose a date in the future',
+        messageColor: 'white',
+        backgroundColor: 'red',
+        position: 'topCenter',
+        timeout: 3000,
+      });
+    } else {
+      refs.startBtn.removeAttribute('disabled');
+      userSelectedDate = selectedDates[0];
+    }
   },
 };
 
 flatpickr(refs.inputEl, options);
 
-let userSelectedDate;
-
-const selectDate = new Date(refs.inputEl.value);
-
 refs.startBtn.addEventListener('click', () => {
-  console.log('START');
-
+  refs.inputEl.setAttribute('disabled', '');
+  refs.startBtn.setAttribute('disabled', '');
   const intervalID = setInterval(() => {
     const currentDate = new Date();
-    const ms = selectDate - currentDate;
-    console.log(ms);
-    // renderTime(ms);
+    const ms = userSelectedDate - currentDate;
+    renderTime(ms);
 
     if (ms < 1000) {
       clearInterval(intervalID);
+      refs.inputEl.removeAttribute('disabled');
+      refs.startBtn.removeAttribute('disabled');
     }
   }, 1000);
 });
 
+function renderTime(ms) {
+  const parsedTime = convertMs(ms);
+  const { days, hours, minutes, seconds } = parsedTime;
+  elements.days.textContent = days.toString().padStart(2, '0');
+  elements.hours.textContent = hours.toString().padStart(2, '0');
+  elements.minutes.textContent = minutes.toString().padStart(2, '0');
+  elements.seconds.textContent = seconds.toString().padStart(2, '0');
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
